295208c031037a8b3cc3db392c22aacaf18db4c1
Ahmad Arab <ahmadarab45521@gmail.com>
2022-11-01 18:51:57 +0200

    js

--- UI/wwwroot/js/site.js
+++ UI/wwwroot/js/site.js
@@ -2,3 +2,13 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+function addRow(tableID) {
+    var table = document.getElementById("infoTable");
+    var rowCount = table.rows.length;
+    var row = table.insertRow(rowCount);
+    //Column 1.
+    var cell1 = row.insertCell(0);
+    var element1 = document.createElement(“input”);
+    element1.type = “button”;
+}
